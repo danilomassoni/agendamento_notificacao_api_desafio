c4fbd6511d226a3de65b112ae91e06c787130cbf
danilomassoni <massonidanilo@gmail.com>
2025-02-23 13:48:22 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/danilomassoni/agendamento_notificacao_api_desafio/AgendamentoNotificacaoApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.danilomassoni.agendamento_notificacao_api_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoNotificacaoApiDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilomassoni/agendamento_notificacao_api_desafio/AgendamentoNotificacaoApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilomassoni.agendamento_notificacao_api_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoNotificacaoApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
