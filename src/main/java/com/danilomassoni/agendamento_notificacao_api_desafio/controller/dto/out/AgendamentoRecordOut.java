56aa0c39920627449f82e78e019806110365e54c
danilomassoni <massonidanilo@gmail.com>
2025-02-24 13:15:48 -0300

    Deixando mais claro as classes

--- src/main/java/com/danilomassoni/agendamento_notificacao_api_desafio/controller/dto/out/AgendamentoRecordOut.java
+++ src/main/java/com/danilomassoni/agendamento_notificacao_api_desafio/controller/dto/out/AgendamentoRecordOut.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import com.danilomassoni.agendamento_notificacao_api_desafio.infrastructure.enums.StatusNotificacaoEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+// A classe AgendamentoRecordOut é responsável por representar o objeto de resposta da API de agendamento de notificações.
 public record AgendamentoRecordOut(Long id,
                                 String emailDestinatario, 
                                 String telefoneDestinatario,
