349f8e22f707c742856d64cee932d73f9a2b5662
danilomassoni <massonidanilo@gmail.com>
2025-02-24 20:59:49 -0300

    Adicionado método de cancelamento

--- src/main/java/com/danilomassoni/agendamento_notificacao_api_desafio/business/AgendamentoService.java
+++ src/main/java/com/danilomassoni/agendamento_notificacao_api_desafio/business/AgendamentoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.danilomassoni.agendamento_notificacao_api_desafio.business.mapper.IAgendamentoMapper;
 import com.danilomassoni.agendamento_notificacao_api_desafio.controller.dto.in.AgendamentoRecord;
 import com.danilomassoni.agendamento_notificacao_api_desafio.controller.dto.out.AgendamentoRecordOut;
+import com.danilomassoni.agendamento_notificacao_api_desafio.infrastructure.entity.Agendamento;
 import com.danilomassoni.agendamento_notificacao_api_desafio.infrastructure.exception.NotFoundException;
 import com.danilomassoni.agendamento_notificacao_api_desafio.infrastructure.repositories.AgandemantoRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,13 @@ public class AgendamentoService {
     public AgendamentoRecordOut buscarAgendamentosPorId(Long id) {
         return agendamentoMapper.paraOut(repository.findById(id)
             .orElseThrow(() -> new NotFoundException("Id não encontrado")));
+    }
+
+    public void cancelarAgendamento(Long id) {
+        Agendamento agendamento = repository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Id não encontrado"));
+        
+        repository.save(agendamentoMapper.paraEntityCancelamento(agendamento));
     }
 
     
