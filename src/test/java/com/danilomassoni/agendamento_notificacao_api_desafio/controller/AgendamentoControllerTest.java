a1e850bc04e0d29f646fea6fa6f188832cd962f6
danilomassoni <massonidanilo@gmail.com>
2025-02-26 18:35:31 -0300

    Criação do módulo TestController

--- src/test/java/com/danilomassoni/agendamento_notificacao_api_desafio/controller/AgendamentoControllerTest.java
+++ src/test/java/com/danilomassoni/agendamento_notificacao_api_desafio/controller/AgendamentoControllerTest.java
@@ -3,15 +3,20 @@ package com.danilomassoni.agendamento_notificacao_api_desafio.controller;
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.danilomassoni.agendamento_notificacao_api_desafio.business.AgendamentoService;
 import com.danilomassoni.agendamento_notificacao_api_desafio.controller.dto.in.AgendamentoRecord;
 import com.danilomassoni.agendamento_notificacao_api_desafio.controller.dto.out.AgendamentoRecordOut;
 import com.danilomassoni.agendamento_notificacao_api_desafio.infrastructure.enums.StatusNotificacaoEnum;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @ExtendWith(MockitoExtension.class)
 public class AgendamentoControllerTest {
@@ -22,17 +27,33 @@ public class AgendamentoControllerTest {
     @Mock 
     AgendamentoService service;
 
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    
+
     private AgendamentoRecord agendamentoRecord;
     private AgendamentoRecordOut agendamentoRecordOut;
 
     @BeforeEach
-    void setup{
+    void setup(){
+
+        mockMvc = MockMvcBuilders.standaloneSetup(agendamentoController).build();
+        
+        objectMapper.registerModule(new JavaTimeModule());
+
         agendamentoRecord = new AgendamentoRecord("email@email.com", "5517991658256",
              "Mensagem de teste", LocalDateTime.of(2025, 1, 2, 11, 1, 1));     
              
         agendamentoRecordOut = new AgendamentoRecordOut(1L, "email@email.com", "5517991698260",
               "Mensagem de teste saída", LocalDateTime.of(2025, 1, 2, 11, 1, 1), 
               StatusNotificacaoEnum.AGENDADO);
+    }
+
+    @Test
+    void deveCriarAgendamentoComSucesso(){
+        when()
     }
 
     
